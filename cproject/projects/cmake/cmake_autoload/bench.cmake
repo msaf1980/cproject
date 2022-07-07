06f1fd62e20a894757afe91f92329b958a7d658d
msaf1980 <msaf1980@gmail.com>
2022-07-07 19:24:03 +0500

    cmake project: add bench test for BUILD_BENCH

--- cproject/projects/cmake/cmake_autoload/bench.cmake
+++ cproject/projects/cmake/cmake_autoload/bench.cmake
@@ -1,13 +1,15 @@
-if(NOT TARGET bench)
-    add_custom_target(bench)
-endif()
+if(BUILD_BENCH)
+    if(NOT TARGET bench)
+        add_custom_target(bench)
+    endif()
 
-# Add benchmark to  bench target
-function(custom_add_bench targetname commandname)
-    add_custom_target(
-        ${targetname}
-        COMMAND ${commandname}
-        COMMENT "Running benchmark ${targetname}")
-    add_dependencies(${targetname} ${targetname})
-    add_dependencies(bench ${targetname})
-endfunction()
+    # Add benchmark to  bench target
+    function(custom_add_bench targetname commandname)
+        add_custom_target(
+            ${targetname}
+            COMMAND ${commandname}
+            COMMENT "Running benchmark ${targetname}")
+        add_dependencies(${targetname} ${targetname})
+        add_dependencies(bench ${targetname})
+    endfunction()
+endif()
