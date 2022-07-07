fe2d314413d81fd8674201b3609680ec9a41abc2
msaf1980 <msaf1980@gmail.com>
2022-07-07 18:43:36 +0500

    cmake project: try to auto add default project structure

--- cproject/projects/cmake/targets.cmake
+++ cproject/projects/cmake/targets.cmake
@@ -1,1 +1,26 @@
 # Add target subdirectories
+
+# Default includes
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/include")
+    add_subdirectory(include)
+endif()
+
+# Default sources (for libs or apps, if no internal libs)
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/src/CMakeLists.txt")
+    add_subdirectory(src)
+endif()
+
+# Default sources (for apps)
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/apps/CMakeLists.txt")
+    add_subdirectory(apps)
+endif()
+
+# Default docs
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/docs/CMakeLists.txt")
+    add_subdirectory(docs)
+endif()
+
+# Default tests
+if(BUILD_TESTING AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/tests/CMakeLists.txt")
+    add_subdirectory(tests)
+endif()
