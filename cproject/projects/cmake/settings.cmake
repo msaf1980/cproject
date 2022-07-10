3ceea04097649a3a71713e173f12f423eab2269c
msaf1980 <msaf1980@gmail.com>
2022-07-10 14:29:06 +0500

    cmake: c/c++ standart version nuumber vars

--- cproject/projects/cmake/settings.cmake
+++ cproject/projects/cmake/settings.cmake
@@ -1,9 +1,12 @@
 set(PROJECT {{PROJECT}})
 set(VERSION {{VERSION}})
 
+SET(CXX_STD_VERSION{%if {{ CXX_STD }} %} {{ CXX_STD }}{%endif%})
 SET(CXX_STD{%if {{ CXX_STD }} %} -std=c++{{ CXX_STD }}{%endif%})
-SET(C_STD{%if {{ C_STD }} %} -std=c{{ C_STD }}{%endif%})
 set(CXX_COMPILE_FEATURES{%if {{ CXX_STD }} %} cxx_std_{{ CXX_STD }}{%endif%})
+
+SET(C_STD_VERSION{%if {{ C_STD }} %} {{ C_STD }}{%endif%})
+SET(C_STD{%if {{ C_STD }} %} -std=c{{ C_STD }}{%endif%})
 set(C_COMPILE_FEATURES{%if {{ C_STD }} %} c_std_{{ C_STD }}{%endif%})
 
 if(CMAKE_CURRENT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
