c810890cbf04a42350170ddfebdf801bbe5071e3
msaf1980 <msaf1980@gmail.com>
2022-07-10 12:23:50 +0500

    cmake apps and header-only library

---
+++ cproject/project_addons/cmake/header_cpp/include/{{PROJECT}}/{{PROJECT}}.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace {{PROJECT}}
+{
+/**
+ * @brief Return the name of this header-only library
+ */
+inline std::string name()
+{
+    return "{{PROJECT}}";
+}
+}  // namespace {{PROJECT}}
